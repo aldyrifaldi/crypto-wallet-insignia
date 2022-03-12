99ad50978127ee0dbb1292357a35fcd61a658102
aldyrifaldi <aldyrifaldi060621@gmail.com>
2022-03-12 10:16:16 +0800

    change a comment

--- routes/balance.js
+++ routes/balance.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const BalanceController = require('../app/controllers/BalanceController.js')
 const JwtMiddleware = require('../app/middlewares/JwtMiddleware')
 
-// topup balance authenticated user
+// read balance authenticated user
 router.get('/', JwtMiddleware,BalanceController.index);
 
 module.exports = router;
