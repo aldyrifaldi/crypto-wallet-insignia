2d93193e126e90239f6d59046c2539ce2dafc4c4
aldyrifaldi <aldyrifaldi060621@gmail.com>
2022-03-12 19:11:28 +0800

    remove try catch

--- app/controllers/TopupController.js
+++ app/controllers/TopupController.js
@@ -3,49 +3,43 @@ const prisma = new PrismaClient()
 
 module.exports = {
     store: async (req,res) => {
-        try {
-            if (parseInt(req.body.amount) <= 0) 
-            return res.status(400).json({
-                status: "bad request",
-                message: "Invalid topup amount"
-            })
+        if (parseInt(req.body.amount) <= 0) 
+        return res.status(400).json({
+            status: "bad request",
+            message: "Invalid topup amount"
+        })
 
-            if (parseInt(req.body.amount) >= 10000000)
-            return res.status(400).json({
-                status: "bad request",
-                message: "amount must less then 10000000 are allowed"
-            })
+        if (parseInt(req.body.amount) >= 10000000)
+        return res.status(400).json({
+            status: "bad request",
+            message: "amount must less then 10000000 are allowed"
+        })
 
-            const user = await prisma.User.update({
-                where: {
-                    id: req.user.id
-                },
-                data: {
-                    balance: {
-                        update: {
-                            balance: {
-                                increment: req.body.amount
-                            }
-                        } 
-                    }
+        const user = await prisma.User.update({
+            where: {
+                id: req.user.id
+            },
+            data: {
+                balance: {
+                    update: {
+                        balance: {
+                            increment: req.body.amount
+                        }
+                    } 
                 }
-            })
+            }
+        })
 
 
-            await prisma.balanceLog.create({
-                data: {
-                    userId: user.id,
-                    status: "DEBIT",
-                    amount: req.body.amount
-                }
-            })
-            
-            return res.status(204).json()
-        } catch (error) {
-            return res.json({
-                status: "error",
-                errors: error
-            })
-        }
+        await prisma.balanceLog.create({
+            data: {
+                userId: user.id,
+                status: "DEBIT",
+                amount: req.body.amount
+            }
+        })
+        
+        return res.status(204).json()
+       
     }
 }
