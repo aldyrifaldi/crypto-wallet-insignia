f19bcf14fa268c8da244a7930a16d660cf1b2bd5
aldyrifaldi <aldyrifaldi060621@gmail.com>
2022-03-12 14:08:31 +0800

    fixed bug while check user token

--- app/middlewares/JwtMiddleware.js
+++ app/middlewares/JwtMiddleware.js
@@ -2,17 +2,19 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
 module.exports = (req, res, next) => {
+    
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
 
-    if (token == null) return res.sendStatus(401)
-    console.log(token);
+    if (token == null) return res.status(401).json({
+        status: "unauthorized",
+    })
+
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-
-        if (err) return res.sendStatus(403)
-
+        if (err) return res.status(403)
+    
         req.user = user
-
+    
         next()
     })
 }
